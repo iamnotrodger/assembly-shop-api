951f9cfdd9ab22d260cd96f06efa55a16101286b
iamnotrodger <rodger.retanal24@gmail.com>
2021-01-01 14:07:36 -0600

    Finished Project routes

--- src/routes/ProjectRoutes.ts
+++ src/routes/ProjectRoutes.ts
@@ -1,5 +1,53 @@
 import { Router } from 'express';
+import {
+    authenticateAdmin,
+    authenticateMember,
+    authenticateToken,
+} from '../controllers/AuthenticationController';
+import {
+    changeProjectName,
+    getProjects,
+    removeProject,
+} from '../controllers/ProjectController';
+import validateRequest, {
+    deleteProjectSchema,
+    getProjectSchema,
+    postProjectSchema,
+    updateProjectSchema,
+} from '../middleware/validateRequest';
 
 const ProjectRoutes = Router();
+
+ProjectRoutes.get(
+    '',
+    validateRequest(getProjectSchema),
+    authenticateToken,
+    authenticateMember,
+    getProjects,
+);
+
+ProjectRoutes.post(
+    '',
+    validateRequest(postProjectSchema),
+    authenticateToken,
+    authenticateAdmin,
+    getProjects,
+);
+
+ProjectRoutes.post(
+    '/delete',
+    validateRequest(deleteProjectSchema),
+    authenticateToken,
+    authenticateAdmin,
+    removeProject,
+);
+
+ProjectRoutes.put(
+    '/update-name',
+    validateRequest(updateProjectSchema),
+    authenticateToken,
+    authenticateAdmin,
+    changeProjectName,
+);
 
 export default ProjectRoutes;
